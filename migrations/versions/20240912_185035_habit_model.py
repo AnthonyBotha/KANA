74c7b1b547f02d8f94cafd51090d38fd85b9b886
Ashton <ashtonhowa@gmail.com>
2024-09-12 19:40:13 -0400

    habits model done

--- migrations/versions/20240912_185035_habit_model.py
+++ migrations/versions/20240912_185035_habit_model.py
@@ -9,6 +9,10 @@ from alembic import op
 import sqlalchemy as sa
 
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 # revision identifiers, used by Alembic.
 revision = '6b4832e6687b'
 down_revision = '5d5c9baac40e'
@@ -31,6 +35,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE habits SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
