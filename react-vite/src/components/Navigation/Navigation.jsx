6618eb10ef532e5e372ad6de499197141b1d0b41
kalivancleave <kalivancleave@gmail.com>
2024-09-13 11:11:53 -0700

    navbar log in and user icon updates based on session status

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,9 +1,36 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import LoginFormModal from "../LoginFormModal";
+import OpenModalButton from '../OpenModalButton';
 import logo from '../../static/KANA.png'
 import "./Navigation.css";
 
 function Navigation() {
+  const sessionUser = useSelector(state => state.session.user)
+
+  const navLinks = sessionUser ?
+    (
+      <div>
+        <div className="displayFlex largeLeftMargin">
+          <p className="fontLight largeFont whiteFont">Tasks</p>
+          <p className="fontLight largeFont whiteFont leftMargin">Inventory</p>
+        </div>
+        <li>
+          <ProfileButton user={sessionUser} />
+        </li>
+      </div>
+    )
+    :
+    (
+      <li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
+      </li>
+    )
+
   return (
     <ul className="dropShadow purple noMargin removeDecorations displayFlex spaceBetween alignCenter">
       <li className="displayFlex alignCenter">
@@ -12,14 +39,10 @@ function Navigation() {
             <img className="logo" src={logo} alt="logo"/>
           </a>
         </NavLink>
-        <div className="displayFlex largeLeftMargin">
-          <p className="fontLight largeFont whiteFont">Tasks</p>
-          <p className="fontLight largeFont whiteFont leftMargin">Inventory</p>
-        </div>
       </li>
 
-      <li>
-        <ProfileButton />
+      <li className="rightPageBorder">
+        {navLinks}
       </li>
     </ul>
   );
