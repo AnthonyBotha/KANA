01b97a94e40070a4e9ae6cf28d33e727f5f3f40c
kalivancleave <kalivancleave@gmail.com>
2024-09-14 05:30:38 -0700

    fixed syntax errors in text

--- react-vite/src/components/WelcomePage/Welcome.jsx
+++ react-vite/src/components/WelcomePage/Welcome.jsx
@@ -7,7 +7,7 @@ function WelcomePage() {
       {/* left side of the page */}
       <div className="halfScreen leftPageBorder fullPadding">
         <p className="font xx-largeFont whiteFont bold">Motivate yourself to achieve your goals.</p>
-        <p className="font smallFont whiteFont">It's time to have fun when you get things done!
+        <p className="font smallFont whiteFont">It&apos;s time to have fun when you get things done!
           KANA users improve their life one task at a time.
         </p>
       </div>
