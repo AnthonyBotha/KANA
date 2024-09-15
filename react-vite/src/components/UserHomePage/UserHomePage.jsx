43223013f16649853b2eb2b8a62f623342aec07e
kalivancleave <kalivancleave@gmail.com>
2024-09-15 06:47:05 -0700

    fixed bug where sign up also reroutes to /home

--- react-vite/src/components/UserHomePage/UserHomePage.jsx
+++ react-vite/src/components/UserHomePage/UserHomePage.jsx
@@ -1,7 +1,16 @@
+import SmallWhiteLogo from '../../static/SmallLogoWhite.png';
+
 function UserHomePage() {
   return (
     <>
       <h1>Welcome Home, User!</h1>
+
+      {/* footer */}
+      <div className="purple displayFlex alignBottom spaceBetween littleBottomPadding">
+        <p className='leftPageBorder font whiteFont smallFont noMargin'>© 2024 KANA. All rights reserved.</p>
+        <img className="smallLogo" src={SmallWhiteLogo} />
+        <a className="rightPageBorder fontLight whiteFont smallFont" href='https://github.com/AnthonyBotha/KANA/wiki'>GitHub</a>
+      </div>
     </>
   )
 }
