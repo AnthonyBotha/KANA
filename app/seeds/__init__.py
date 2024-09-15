56272a69b66d96f65177f4cc326ca15da43ff6d9
AnthonyBotha <anthony.botha@outlook.com>
2024-09-14 22:11:07 -0700

    Avatar Parts Seed Data Added

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,12 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .parts.bodies import undo_bodies,seed_bodies
 from .parts.antennas import undo_antennas,seed_antennas
+from .parts.ears import undo_ears,seed_ears
+from .parts.eyes import undo_eyes,seed_eyes
+from .parts.heads import undo_heads,seed_heads
+from .parts.mouths import undo_mouths,seed_mouths
+from .parts.necks import undo_necks,seed_necks
+from .parts.noses import undo_noses,seed_noses
 
 from app.models.db import db, environment, SCHEMA
 
@@ -21,9 +27,22 @@ def seed():
         undo_users()
         undo_bodies()
         undo_antennas()
+        undo_ears()
+        undo_eyes()
+        undo_heads()
+        undo_mouths()
+        undo_necks()
+        undo_noses()
+        
     seed_users()
     seed_bodies()
     seed_antennas()
+    seed_ears()
+    seed_eyes()
+    seed_heads()
+    seed_mouths()
+    seed_necks()
+    seed_noses()
     # Add other seed functions here
 
 
@@ -33,4 +52,10 @@ def undo():
     undo_users()
     undo_bodies()
     undo_antennas()
+    undo_ears()
+    undo_eyes()
+    undo_heads()
+    undo_mouths()
+    undo_necks()
+    undo_noses()
     # Add other undo functions here
