f69f09b763dc04aca8dd39fd12975bc96c3f4e9b
Nicolas Borromeo <nicoborrom@gmail.com>
2024-09-16 16:08:35 -0700

    seeded tags only - no association working yet

---
+++ app/seeds/tags.py
@@ -0,0 +1,44 @@
+from app.models import db, Tag, Habit, Todo, Daily, environment, SCHEMA
+from sqlalchemy.sql import text
+
+def seed_tags():
+    work = Tag(
+        tag_name='Work'
+    )
+    excercise = Tag(
+        tag_name='Excercise'
+    )
+    health = Tag(
+        tag_name='health'
+    )
+    school = Tag(
+        tag_name='school'
+    )
+    teams = Tag(
+        tag_name='teams'
+    )
+    chores = Tag(
+        tag_name='chores'
+    )
+    creativity = Tag(
+        tag_name='creativity'
+    )
+    
+    tags = [work,excercise,health, school, teams, chores, creativity]
+    for tag in tags:
+        db.session.add(tag)
+    db.session.commit()
+
+
+    habit1 = Habit.query.filter_by(title='Add a task to KANA')
+    habit1.tags.append(work)
+
+def undo_tags():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.tasks_tags RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.tags RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM tasks_tags"))
+        db.session.execute(text("DELETE FROM tags"))
+
+        db.session.commit()
