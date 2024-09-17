4e4570817707aaeffcf57e18a7cb3801eb38459f
kalivancleave <kalivancleave@gmail.com>
2024-09-16 18:22:16 -0700

    started avatar modal

---
+++ react-vite/src/components/Avatar/AvatarModal.jsx
@@ -0,0 +1,32 @@
+import { useModal } from '../../context/Modal';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
+function AvatarModal() {
+  const dispatch = useDispatch();
+  const { closeModal } = useModal();
+  const [errors, setErrors] = useState({});
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+
+  const serverResponse = await dispatch(
+    // Search for server things
+  );
+
+  if (serverResponse) {
+    setErrors(serverResponse);
+  } else {
+    closeModal();
+  }
+
+
+  return (
+    <>
+      <h1>Anthony's Avatars</h1>
+    </>
+  )
+}
+
+export default AvatarModal;
